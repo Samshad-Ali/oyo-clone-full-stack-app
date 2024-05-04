95209d7a2bc1c065c6806d7c8aaf3e5c31cc7241
Samshad Ali <sam606166@gmail.com>
2024-05-04 08:52:54 +0530

    d

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 export const dbConnect = async () => {
   try {
+    console.log(process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
     console.log("database connected...!");
   } catch (error) {
